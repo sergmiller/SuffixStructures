59ca52b60dd89cde5428ce5c2e33092e64efdc08
sergmiller <sergey.miller@phystech.edu>
2015-11-16 20:32:28 +0300

    create IS dir

---
+++ Induced Sorting/Induced Sorting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Induced Sorting
+//
+//  Created by Сергей Миллер on 16.11.15.
+//  Copyright © 2015 Сергей Миллер. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
